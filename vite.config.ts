0d9d2f93783c668184f2d6ebe5c9996a3dafaeca
seran321 <159777752+seran321@users.noreply.github.com>
2025-07-10 06:33:08 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    watch: {
+      usePolling: false,
+      interval: 300,
+      binaryInterval: 1000,
+      ignored: ['**/node_modules/**', '**/.git/**']
+    }
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
